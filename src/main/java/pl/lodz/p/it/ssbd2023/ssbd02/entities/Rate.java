1ac940bd52bf030e56d11abd6e808a1bdb849f26
229954 Marszał Jakub <229954@edu.p.lodz.pl>
2023-03-21 21:05:37 +0100

    SSBD202302-17 Add implementation of classes Person, Rate and Company

--- src/main/java/pl/lodz/p/it/ssbd2023/ssbd02/entities/Rate.java
+++ src/main/java/pl/lodz/p/it/ssbd2023/ssbd02/entities/Rate.java
@@ -1,4 +1,17 @@
 package pl.lodz.p.it.ssbd2023.ssbd02.entities;
 
-public class Rate {
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+@Data
+@SuperBuilder
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(callSuper = true)
+public class Rate extends AbstractEntity {
+    private Integer value;
+    private Person person;
 }
