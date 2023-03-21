ca7b1359a2cca4c43e79fcd8d9259c858f79a193
229872 Dygasiński Bartłomiej <229872@edu.p.lodz.pl>
2023-03-21 15:57:34 +0100

    SSBD202302-16 Implement with jpa annotations required classes

--- src/main/java/pl/lodz/p/it/ssbd2023/ssbd02/entities/AbstractEntity.java
+++ src/main/java/pl/lodz/p/it/ssbd2023/ssbd02/entities/AbstractEntity.java
@@ -1,4 +1,15 @@
 package pl.lodz.p.it.ssbd2023.ssbd02.entities;
 
-public class AbstractEntity {
+import jakarta.persistence.*;
+import lombok.Getter;
+
+@MappedSuperclass
+@Getter
+public abstract class AbstractEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+    @Version
+    private Long version;
 }
