a2a617f260c4f4e9bd2d436676a282acb7251792
236555 Mikołaj Kacprzyk <236555@edu.p.lodz.pl>
2023-03-21 19:43:21 +0100

    SSBD202302-18 Implemented ProductGroup class.

--- src/main/java/pl/lodz/p/it/ssbd2023/ssbd02/entities/ProductGroup.java
+++ src/main/java/pl/lodz/p/it/ssbd2023/ssbd02/entities/ProductGroup.java
@@ -1,4 +1,23 @@
 package pl.lodz.p.it.ssbd2023.ssbd02.entities;
 
-public class ProductGroup {
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+import java.util.Collection;
+
+@Data
+@SuperBuilder
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(callSuper = true)
+public class ProductGroup extends AbstractEntity {
+    private Boolean archive;
+    private String name;
+    private Double averageRating;
+    private Collection<Product> products;
+    private Collection<Rate> rates;
+    private Category category;
 }
