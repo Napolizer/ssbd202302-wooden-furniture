f7f98abd6ad52427b147972ce4d8f39b477ae9fc
236555 Mikołaj Kacprzyk <236555@edu.p.lodz.pl>
2023-04-02 20:09:51 +0200

    SSBD202302-64 Implemented AccountService methods to retrieve account information.

--- src/main/java/pl/lodz/p/it/ssbd2023/ssbd02/mok/service/impl/AccountService.java
+++ src/main/java/pl/lodz/p/it/ssbd2023/ssbd02/mok/service/impl/AccountService.java
@@ -2,8 +2,22 @@ package pl.lodz.p.it.ssbd2023.ssbd02.mok.service.impl;
 
 import jakarta.ejb.Stateless;
 import jakarta.inject.Inject;
+import pl.lodz.p.it.ssbd2023.ssbd02.entities.Account;
+import pl.lodz.p.it.ssbd2023.ssbd02.entities.Person;
+import pl.lodz.p.it.ssbd2023.ssbd02.mok.facade.api.PersonFacadeOperations;
+
+import java.util.Optional;
 
 @Stateless
 public class AccountService {
+    @Inject
+    private PersonFacadeOperations personFacadeOperations;
 
+    public Optional<Account> getAccountByLogin(String login) {
+        return personFacadeOperations.findByAccountLogin(login).map(Person::getAccount);
+    }
+
+    public Optional<Account> getAccountById(Long id) {
+        return personFacadeOperations.find(id).map(Person::getAccount);
+    }
 }
