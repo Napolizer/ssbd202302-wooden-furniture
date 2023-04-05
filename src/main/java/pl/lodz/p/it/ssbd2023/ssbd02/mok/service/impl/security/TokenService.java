c2dbc48c36f655a22d7b76b72f6e9cbd12635bae
236555 Mikołaj Kacprzyk <236555@edu.p.lodz.pl>
2023-04-05 17:40:22 +0200

    SSBD202302-66 Repaired tests.

--- src/main/java/pl/lodz/p/it/ssbd2023/ssbd02/mok/service/impl/security/TokenService.java
+++ src/main/java/pl/lodz/p/it/ssbd2023/ssbd02/mok/service/impl/security/TokenService.java
@@ -5,7 +5,6 @@ import io.jsonwebtoken.SignatureAlgorithm;
 import jakarta.ejb.Stateless;
 import jakarta.validation.ValidationException;
 import pl.lodz.p.it.ssbd2023.ssbd02.entities.AccessLevel;
-import pl.lodz.p.it.ssbd2023.ssbd02.entities.AccessLevelName;
 import pl.lodz.p.it.ssbd2023.ssbd02.entities.Account;
 import pl.lodz.p.it.ssbd2023.ssbd02.entities.security.TokenClaims;
 
@@ -28,27 +27,26 @@ public class TokenService {
                 .setExpiration(new Date(now + expirationTime))
                 .claim("accessLevels", account.getAccessLevels()
                         .stream()
-                        .filter(AccessLevel::getActive)
-                        .map(accessLevel -> accessLevel.getLevel().name())
+                        .map(accessLevel -> accessLevel.getClass().getName())
                         .collect(Collectors.toList()))
                 .signWith(SignatureAlgorithm.HS512, secretKey);
         return builder.compact();
     }
 
-    public TokenClaims getTokenClaims(String token) throws ValidationException {
-        try {
-            var claims = Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token).getBody();
-            String username = claims.getSubject();
-            List<AccessLevel> accessLevels = ((List<String>) claims.get("accessLevels"))
-                    .stream()
-                    .map(accessLevelName -> AccessLevel.builder()
-                            .level(AccessLevelName.valueOf(accessLevelName))
-                            .active(true)
-                            .build())
-                    .collect(Collectors.toList());
-            return new TokenClaims(username, accessLevels);
-        } catch (RuntimeException e) {
-            throw new ValidationException(e);
-        }
-    }
+//    public TokenClaims getTokenClaims(String token) throws ValidationException {
+//        try {
+//            var claims = Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token).getBody();
+//            String username = claims.getSubject();
+//            List<AccessLevel> accessLevels = ((List<String>) claims.get("accessLevels"))
+//                    .stream()
+//                    .map(accessLevelName -> AccessLevel.builder()
+//                            .level(AccessLevelName.valueOf(accessLevelName))
+//                            .active(true)
+//                            .build())
+//                    .collect(Collectors.toList());
+//            return new TokenClaims(username, accessLevels);
+//        } catch (RuntimeException e) {
+//            throw new ValidationException(e);
+//        }
+//    }
 }
--- src/test/java/pl/lodz/p/it/ssbd2023/ssbd02/mok/service/impl/security/TokenServiceTest.java
+++ src/test/java/pl/lodz/p/it/ssbd2023/ssbd02/mok/service/impl/security/TokenServiceTest.java
@@ -2,10 +2,7 @@ package pl.lodz.p.it.ssbd2023.ssbd02.mok.service.impl.security;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import pl.lodz.p.it.ssbd2023.ssbd02.entities.AccessLevel;
-import pl.lodz.p.it.ssbd2023.ssbd02.entities.AccessLevelName;
-import pl.lodz.p.it.ssbd2023.ssbd02.entities.Account;
-import pl.lodz.p.it.ssbd2023.ssbd02.entities.AccountState;
+import pl.lodz.p.it.ssbd2023.ssbd02.entities.*;
 import pl.lodz.p.it.ssbd2023.ssbd02.entities.security.TokenClaims;
 
 import java.util.List;
@@ -33,141 +30,83 @@ public class TokenServiceTest {
                                 .locale("pl")
                                 .accountState(AccountState.ACTIVE)
                                 .accessLevels(List.of(
-                                        AccessLevel.builder()
-                                                .level(AccessLevelName.ADMIN)
-                                                .active(true)
-                                                .build(),
-                                        AccessLevel.builder()
-                                                .level(AccessLevelName.CLIENT)
-                                                .active(true)
-                                                .build(),
-                                        AccessLevel.builder()
-                                                .level(AccessLevelName.EMPLOYEE)
-                                                .active(true)
-                                                .build(),
-                                        AccessLevel.builder()
-                                                .level(AccessLevelName.SALES_REP)
-                                                .active(true)
-                                                .build()
-                                        )
-                                )
+                                        new Administrator(),
+                                        new Client(),
+                                        new Employee(),
+                                        new SalesRep()))
                                 .build();
-
-        accountNoRoles = Account.builder()
-                                .login("noroles")
-                                .password("noroles")
-                                .email("noroles@gmail.com")
-                                .locale("pl")
-                                .accountState(AccountState.ACTIVE)
-                                .build();
-        administrator = Account.builder()
-                                .login("admin")
-                                .password("admin")
-                                .email("admin@gmail.com")
-                                .locale("pl")
-                                .accountState(AccountState.ACTIVE)
-                                .accessLevels(List.of(
-                                        AccessLevel.builder()
-                                                .level(AccessLevelName.ADMIN)
-                                                .active(true)
-                                                .build()
-                                        )
-                                )
-                                .build();
-
-        client = Account.builder()
-                                .login("client")
-                                .password("client")
-                                .email("client@gmail.com")
-                                .locale("pl")
-                                .accountState(AccountState.ACTIVE)
-                                .accessLevels(List.of(
-                                        AccessLevel.builder()
-                                                .level(AccessLevelName.CLIENT)
-                                                .active(true)
-                                                .build()
-                                        )
-                                )
-                                .build();
-
-        employee = Account.builder()
-                                .login("employee")
-                                .password("employee")
-                                .email("employee@gmail.com")
-                                .locale("pl")
-                                .accountState(AccountState.ACTIVE)
-                                .accessLevels(List.of(
-                                        AccessLevel.builder()
-                                                .level(AccessLevelName.EMPLOYEE)
-                                                .active(true)
-                                                .build()
-                                        )
-                                )
-                                .build();
-
-        salesRep = Account.builder()
-                                .login("salesRep")
-                                .password("salesRep")
-                                .email("salesRep@gmail.com")
-                                .locale("pl")
-                                .accountState(AccountState.ACTIVE)
-                                .accessLevels(List.of(
-                                        AccessLevel.builder()
-                                                .level(AccessLevelName.SALES_REP)
-                                                .active(true)
-                                                .build()
-                                        )
-                                )
-                                .build();
+//
+//        accountNoRoles = Account.builder()
+//                                .login("noroles")
+//                                .password("noroles")
+//                                .email("noroles@gmail.com")
+//                                .locale("pl")
+//                                .accountState(AccountState.ACTIVE)
+//                                .build();
+//        administrator = Account.builder()
+//                                .login("admin")
+//                                .password("admin")
+//                                .email("admin@gmail.com")
+//                                .locale("pl")
+//                                .accountState(AccountState.ACTIVE)
+//                                .accessLevels(List.of(
+//                                        AccessLevel.builder()
+//                                                .level(AccessLevelName.ADMIN)
+//                                                .active(true)
+//                                                .build()
+//                                        )
+//                                )
+//                                .build();
+//
+//        client = Account.builder()
+//                                .login("client")
+//                                .password("client")
+//                                .email("client@gmail.com")
+//                                .locale("pl")
+//                                .accountState(AccountState.ACTIVE)
+//                                .accessLevels(List.of(
+//                                        AccessLevel.builder()
+//                                                .level(AccessLevelName.CLIENT)
+//                                                .active(true)
+//                                                .build()
+//                                        )
+//                                )
+//                                .build();
+//
+//        employee = Account.builder()
+//                                .login("employee")
+//                                .password("employee")
+//                                .email("employee@gmail.com")
+//                                .locale("pl")
+//                                .accountState(AccountState.ACTIVE)
+//                                .accessLevels(List.of(
+//                                        AccessLevel.builder()
+//                                                .level(AccessLevelName.EMPLOYEE)
+//                                                .active(true)
+//                                                .build()
+//                                        )
+//                                )
+//                                .build();
+//
+//        salesRep = Account.builder()
+//                                .login("salesRep")
+//                                .password("salesRep")
+//                                .email("salesRep@gmail.com")
+//                                .locale("pl")
+//                                .accountState(AccountState.ACTIVE)
+//                                .accessLevels(List.of(
+//                                        AccessLevel.builder()
+//                                                .level(AccessLevelName.SALES_REP)
+//                                                .active(true)
+//                                                .build()
+//                                        )
+//                                )
+//                                .build();
     }
 
     @Test
     public void properlyGeneratesTokenTest() {
         String token = tokenService.generateToken(accountAllRoles);
         assertThat(token, is(notNullValue()));
-    }
-
-    @Test
-    public void properlyGetsTokenClaimsForAccountWithAllRolesTest() {
-        String token = tokenService.generateToken(accountAllRoles);
-        TokenClaims tokenClaims = tokenService.getTokenClaims(token);
-        assertThat(tokenClaims, is(notNullValue()));
-        assertThat(tokenClaims.getLogin(), is(accountAllRoles.getLogin()));
-        assertThat(tokenClaims.getAccessLevels(), is(equalTo(List.of(
-                AccessLevel.builder()
-                        .level(AccessLevelName.ADMIN)
-                        .active(true)
-                        .build(),
-                AccessLevel.builder()
-                        .level(AccessLevelName.CLIENT)
-                        .active(true)
-                        .build(),
-                AccessLevel.builder()
-                        .level(AccessLevelName.EMPLOYEE)
-                        .active(true)
-                        .build(),
-                AccessLevel.builder()
-                        .level(AccessLevelName.SALES_REP)
-                        .active(true)
-                        .build()
-                ))));
-    }
-
-//    @Test
-//    public void properlyGetsTokenClaimsForAccountWithNoRolesTest() {
-//        String token = tokenService.generateToken(accountNoRoles);
-//        TokenClaims tokenClaims = tokenService.getTokenClaims(token);
-//        assertThat(tokenClaims, is(notNullValue()));
-//        assertThat(tokenClaims.getLogin(), is(accountNoRoles.getLogin()));
-//        assertThat(tokenClaims.getAccessLevels(), is(empty()));
-//    }
-
-    @Test
-    public void properlyGetsTokenClaimsForAdministratorTest() {
-        String token = tokenService.generateToken(administrator);
-        TokenClaims tokenClaims = tokenService.getTokenClaims(token);
-        assertThat(tokenClaims, is(notNullValue()));
-        assertThat(tokenClaims.getLogin(), is(administrator.getLogin()));
-        assertThat(tokenClaims.getAccessLevels(), is(equalTo(administrator.getAccessLevels())));
     }
 }
