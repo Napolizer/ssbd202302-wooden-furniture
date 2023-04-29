18c0b80de7edd9863bb07cceb0d2e3e16f954f9d
236555 Mikołaj Kacprzyk <236555@edu.p.lodz.pl>
2023-04-29 11:56:43 +0200

    SSBD202302-152 Refactored interceptor classes.

--- src/main/java/pl/lodz/p/it/ssbd2023/ssbd02/interceptors/AccountFacadeExceptionsInterceptor.java
+++ src/main/java/pl/lodz/p/it/ssbd2023/ssbd02/interceptors/AccountFacadeExceptionsInterceptor.java
@@ -7,20 +7,20 @@ import jakarta.persistence.PersistenceException;
 import pl.lodz.p.it.ssbd2023.ssbd02.exceptions.ApplicationExceptionFactory;
 
 public class AccountFacadeExceptionsInterceptor {
-    @AroundInvoke
-    public Object intercept(InvocationContext ictx) throws Exception {
-        try {
-            return ictx.proceed();
-        } catch (OptimisticLockException ole) {
-            throw ole;
-        } catch (PersistenceException pe) {
-            if (pe.getMessage().contains("account_login_key")) {
-                throw ApplicationExceptionFactory.createLoginAlreadyExistsException(pe);
-            } else if (pe.getMessage().contains("account_email_key")) {
-                throw ApplicationExceptionFactory.createEmailAlreadyExistsException(pe);
-            } else {
-                throw pe;
-            }
-        }
+  @AroundInvoke
+  public Object intercept(InvocationContext ictx) throws Exception {
+    try {
+      return ictx.proceed();
+    } catch (OptimisticLockException ole) {
+      throw ole;
+    } catch (PersistenceException pe) {
+      if (pe.getMessage().contains("account_login_key")) {
+        throw ApplicationExceptionFactory.createLoginAlreadyExistsException(pe);
+      } else if (pe.getMessage().contains("account_email_key")) {
+        throw ApplicationExceptionFactory.createEmailAlreadyExistsException(pe);
+      } else {
+        throw pe;
+      }
     }
+  }
 }
