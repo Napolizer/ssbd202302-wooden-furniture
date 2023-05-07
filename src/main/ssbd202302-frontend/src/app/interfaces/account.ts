8d7e13c8dbbe8b596a3cf7c6bf14bba0f03d7765
236555 Mikołaj Kacprzyk <236555@edu.p.lodz.pl>
2023-05-07 20:46:15 +0200

    SSBD202302-182 Refactored account page looks.

--- src/main/ssbd202302-frontend/src/app/interfaces/account.ts
+++ src/main/ssbd202302-frontend/src/app/interfaces/account.ts
@@ -4,12 +4,16 @@ export interface Account {
   accountState: string,
   address: Address,
   archive: boolean,
+  blockadeEnd: Date,
   email: string,
   failedLoginCounter: number,
-  fistName: string,
+  firstName: string,
   groups: string[],
   id: number,
   lastName: string,
+  lastLogin: Date,
+  lastFailedLogin: Date,
+  lastLoginIpAddress: string,
   locale: string,
   login: string
 }
