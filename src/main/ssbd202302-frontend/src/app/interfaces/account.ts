41c50bee0cc4d4bb48007475991e13564890e274
236555 Mikołaj Kacprzyk <236555@edu.p.lodz.pl>
2023-05-07 14:38:26 +0200

    SSBD202302-182 Refactored dto on frontend.

--- src/main/ssbd202302-frontend/src/app/interfaces/account.ts
+++ src/main/ssbd202302-frontend/src/app/interfaces/account.ts
@@ -1,10 +1,15 @@
+import {Address} from "./address";
+
 export interface Account {
   accountState: string,
+  address: Address,
   archive: boolean,
   email: string,
   failedLoginCounter: number,
+  fistName: string,
   groups: string[],
   id: number,
+  lastName: string,
   locale: string,
   login: string
 }
