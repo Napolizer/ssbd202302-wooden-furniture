d97e02d57bbc2b65cb58f729ee1ad45aba9fac4e
236555 Mikołaj Kacprzyk <236555@edu.p.lodz.pl>
2023-05-08 18:14:28 +0200

    SSBD202302-181 Added missing translations on frontend.

--- src/main/ssbd202302-frontend/src/app/components/toolbar/toolbar.component.ts
+++ src/main/ssbd202302-frontend/src/app/components/toolbar/toolbar.component.ts
@@ -1,22 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {NavigationService} from "../../services/navigation.service";
 import {AuthenticationService} from "../../services/authentication.service";
 import {AlertService} from "@full-fledged/alerts";
+import {TranslateService} from "@ngx-translate/core";
+import {first, Subject, takeUntil} from "rxjs";
 
 @Component({
   selector: 'app-toolbar',
   templateUrl: './toolbar.component.html',
   styleUrls: ['./toolbar.component.sass']
 })
-export class ToolbarComponent implements OnInit {
+export class ToolbarComponent implements OnInit, OnDestroy {
+  destroy = new Subject<boolean>();
 
   constructor(
     private alertService: AlertService,
     private navigationService: NavigationService,
-    private authenticationService: AuthenticationService
+    private authenticationService: AuthenticationService,
+    private translate: TranslateService
   ) { }
 
   ngOnInit(): void {
+  }
+
+  ngOnDestroy(): void {
+    this.destroy.next(true);
+    this.destroy.unsubscribe();
   }
 
   redirectToMainPage(): void {
@@ -45,7 +54,11 @@ export class ToolbarComponent implements OnInit {
 
   logout(): void {
     this.authenticationService.logout();
-    this.alertService.success('You have been logged out');
-    this.redirectToMainPage();
+    this.translate.get('toolbar.logout.success.message')
+      .pipe(first(), takeUntil(this.destroy))
+      .subscribe(message => {
+        this.alertService.success(message);
+        this.redirectToMainPage();
+      });
   }
 }
