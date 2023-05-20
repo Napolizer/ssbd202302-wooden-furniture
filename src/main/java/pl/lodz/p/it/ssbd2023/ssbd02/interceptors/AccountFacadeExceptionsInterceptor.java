5567580ec79e8f285173b6b7c5d7a67e488fb562
236555 Mikołaj Kacprzyk <236555@edu.p.lodz.pl>
2023-05-20 09:21:42 +0200

    SSBD202302-247 Fixed checkstyle violations.

--- src/main/java/pl/lodz/p/it/ssbd2023/ssbd02/interceptors/AccountFacadeExceptionsInterceptor.java
+++ src/main/java/pl/lodz/p/it/ssbd2023/ssbd02/interceptors/AccountFacadeExceptionsInterceptor.java
@@ -14,15 +14,15 @@ public class AccountFacadeExceptionsInterceptor {
     } catch (OptimisticLockException ole) {
       throw ole;
     } catch (PersistenceException pe) {
-        if (pe.getMessage().contains("account_login_key")) {
-          throw ApplicationExceptionFactory.createLoginAlreadyExistsException(pe);
-        } else if (pe.getMessage().contains("account_email_key")) {
-          throw ApplicationExceptionFactory.createEmailAlreadyExistsException(pe);
-        } else if (pe.getMessage().contains("company_nip_key")) {
-          throw ApplicationExceptionFactory.createNipAlreadyExistsException(pe);
-        } else {
-          throw pe;
-        }
+      if (pe.getMessage().contains("account_login_key")) {
+        throw ApplicationExceptionFactory.createLoginAlreadyExistsException(pe);
+      } else if (pe.getMessage().contains("account_email_key")) {
+        throw ApplicationExceptionFactory.createEmailAlreadyExistsException(pe);
+      } else if (pe.getMessage().contains("company_nip_key")) {
+        throw ApplicationExceptionFactory.createNipAlreadyExistsException(pe);
+      } else {
+        throw pe;
+      }
     }
   }
 }
