c0f728c8bd7346222cd42a5595662894a481c248
229954 Marszał Jakub <229954@edu.p.lodz.pl>
2023-05-21 16:29:56 +0200

    SSBD202302-261 Add tests for MOK.27 change email address(activate new email from link)

--- src/main/java/pl/lodz/p/it/ssbd2023/ssbd02/mok/dto/SetEmailToSendPasswordDto.java
+++ src/main/java/pl/lodz/p/it/ssbd2023/ssbd02/mok/dto/SetEmailToSendPasswordDto.java
@@ -1,9 +1,13 @@
 package pl.lodz.p.it.ssbd2023.ssbd02.mok.dto;
 
 import jakarta.validation.constraints.Email;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class SetEmailToSendPasswordDto {
   @Email
   private String email;
--- src/test/java/pl/lodz/p/it/ssbd2023/ssbd02/web/InitData.java
+++ src/test/java/pl/lodz/p/it/ssbd2023/ssbd02/web/InitData.java
@@ -2,13 +2,59 @@ package pl.lodz.p.it.ssbd2023.ssbd02.web;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.ws.rs.core.HttpHeaders;
+import pl.lodz.p.it.ssbd2023.ssbd02.entities.Account;
+import pl.lodz.p.it.ssbd2023.ssbd02.entities.TokenType;
 import pl.lodz.p.it.ssbd2023.ssbd02.mok.dto.AccessLevelDto;
 import pl.lodz.p.it.ssbd2023.ssbd02.mok.dto.AccountCreateDto;
 import pl.lodz.p.it.ssbd2023.ssbd02.mok.dto.AccountRegisterDto;
+import pl.lodz.p.it.ssbd2023.ssbd02.mok.dto.SetEmailToSendPasswordDto;
+import pl.lodz.p.it.ssbd2023.ssbd02.mok.dto.UserCredentialsDto;
+import pl.lodz.p.it.ssbd2023.ssbd02.mok.service.impl.security.TokenService;
+
+import static io.restassured.RestAssured.given;
 
 public class InitData {
 
-  private static ObjectMapper objectMapper = new ObjectMapper();
+  private static final ObjectMapper objectMapper = new ObjectMapper();
+  private static final TokenService tokenService = new TokenService();
+
+  private static String retrieveToken(UserCredentialsDto credentials) {
+    return given()
+            .contentType("application/json")
+            .header(HttpHeaders.ACCEPT_LANGUAGE, "pl")
+            .body(InitData.mapToJsonString(credentials))
+            .when()
+            .post("/account/login")
+            .then()
+            .statusCode(200)
+            .contentType("application/json")
+            .extract()
+            .path("token");
+  }
+
+  public static String retrieveVersion(String login) {
+    return given()
+            .contentType("application/json")
+            .header(HttpHeaders.AUTHORIZATION, "Bearer " + retrieveAdminToken())
+            .get("/account/login/" + login)
+            .then()
+            .statusCode(200)
+            .contentType("application/json")
+            .extract()
+            .path("hash");
+  }
+
+  public static int retrieveAccountId(String login) {
+    return given()
+            .header("Authorization", "Bearer " + retrieveAdminToken())
+            .when()
+            .get("/account/login/" + login)
+            .then()
+            .statusCode(200)
+            .extract()
+            .path("id");
+  }
 
   public static String mapToJsonString(Object object) {
     try {
@@ -16,6 +62,29 @@ public class InitData {
     } catch (JsonProcessingException e) {
       throw new RuntimeException();
     }
+  }
+  public static String retrieveAdminToken() {
+    return retrieveToken(InitData.getUserCredentials("administrator", "Student123!"));
+  }
+
+  public static String retrieveClientToken() {
+    return retrieveToken(InitData.getUserCredentials("client", "Student123!"));
+  }
+
+  public static String generateChangeEmailToken(String login, String newEmail) {
+    Account account = Account.builder().newEmail(newEmail).login(login).build();
+    return tokenService.generateTokenForEmailLink(account, TokenType.CHANGE_EMAIL);
+  }
+
+  public static UserCredentialsDto getUserCredentials(String login, String password) {
+    return UserCredentialsDto.builder()
+            .login(login)
+            .password(password)
+            .build();
+  }
+
+  public static SetEmailToSendPasswordDto getEmailToChange(String newEmail) {
+    return new SetEmailToSendPasswordDto(newEmail);
   }
 
   public static AccountRegisterDto getAccountToRegister() {
