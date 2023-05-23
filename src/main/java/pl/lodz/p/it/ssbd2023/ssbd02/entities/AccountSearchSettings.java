0772e26b4670ea0aa00a105e74cab356de402a9f
236534 Glapiński Michał <236534@edu.p.lodz.pl>
2023-05-23 19:29:32 +0200

    SSBD202302-300 Added entity that saves user search preferences

---
+++ src/main/java/pl/lodz/p/it/ssbd2023/ssbd02/entities/AccountSearchSettings.java
@@ -0,0 +1,43 @@
+package pl.lodz.p.it.ssbd2023.ssbd02.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.NamedQueries;
+import jakarta.persistence.NamedQuery;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.experimental.SuperBuilder;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+@Entity
+@SuperBuilder
+@Table(name = "account_search_settings")
+@NamedQueries({
+    @NamedQuery(name = "accountListPreferences.findByAccount",
+                query = "SELECT p FROM AccountSearchSettings p WHERE p.account = :account")
+})
+public class AccountSearchSettings extends AbstractEntity {
+  @NotNull
+  @OneToOne
+  @JoinColumn(name = "account_id")
+  private Account account;
+  @Column(name = "search_page", columnDefinition = "integer default 0")
+  private Integer searchPage;
+  @Column(name = "displayed_accounts", columnDefinition = "integer default 3")
+  private Integer displayedAccounts;
+  @Column(name = "search_keyword", columnDefinition = "varchar(5) default ''")
+  private String searchKeyword;
+  @Column(name = "sort_by")
+  private String sortBy;
+  @Column(name = "sort_order", columnDefinition = "boolean default true")
+  private Boolean sortOrder;
+}
