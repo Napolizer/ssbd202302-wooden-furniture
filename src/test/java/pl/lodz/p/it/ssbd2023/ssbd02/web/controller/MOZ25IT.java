c9237060657d166f92ab09a290ed771f014e0edd
236534 Glapiński Michał <236534@edu.p.lodz.pl>
2023-06-15 23:21:43 +0200

    SSBD202302-433 Add MOZ25 Tests, refactor other tests.

---
+++ src/test/java/pl/lodz/p/it/ssbd2023/ssbd02/web/controller/MOZ25IT.java
@@ -0,0 +1,123 @@
+package pl.lodz.p.it.ssbd2023.ssbd02.web.controller;
+
+import jakarta.annotation.security.RolesAllowed;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.QueryParam;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import java.util.List;
+import org.junit.jupiter.api.*;
+import org.microshed.testing.SharedContainerConfig;
+import org.microshed.testing.jupiter.MicroShedTest;
+import pl.lodz.p.it.ssbd2023.ssbd02.entities.enums.OrderState;
+import pl.lodz.p.it.ssbd2023.ssbd02.moz.dto.order.OrderDto;
+import pl.lodz.p.it.ssbd2023.ssbd02.testcontainers.util.AuthUtil;
+import pl.lodz.p.it.ssbd2023.ssbd02.testcontainers.util.OrderUtil;
+import pl.lodz.p.it.ssbd2023.ssbd02.web.AppContainerConfig;
+import static io.restassured.RestAssured.given;
+import static jakarta.ws.rs.core.HttpHeaders.AUTHORIZATION;
+import static org.hamcrest.Matchers.*;
+import static pl.lodz.p.it.ssbd2023.ssbd02.config.Role.EMPLOYEE;
+import static pl.lodz.p.it.ssbd2023.ssbd02.config.Role.SALES_REP;
+
+@MicroShedTest
+@SharedContainerConfig(AppContainerConfig.class)
+@DisplayName("MOZ.25 - Filter done orders")
+@TestClassOrder(ClassOrderer.OrderAnnotation.class)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class MOZ25IT {
+    public OrderDto order;
+    @Nested
+    @Order(1)
+    @TestClassOrder(ClassOrderer.OrderAnnotation.class)
+    class Positive {
+        @Order(1)
+        @DisplayName("Should properly create order")
+        @Test
+        void shouldProperlyGetCreateOrder() {
+            order = OrderUtil.createOrder(OrderState.DELIVERED);
+            given()
+                    .header(AUTHORIZATION, "Bearer " + AuthUtil.retrieveToken(EMPLOYEE))
+                    .when()
+                    .get("/order/id/%s".formatted(order.getId()))
+                    .then()
+                    .statusCode(200)
+                    .body("id", notNullValue())
+                    .body("orderedProducts", notNullValue())
+                    .body("orderedProducts.amount", notNullValue())
+                    .body("orderedProducts.productId", notNullValue())
+                    .body("orderState", equalTo("DELIVERED"))
+                    .body("recipientFirstName", equalTo("Jan"))
+                    .body("recipientLastName", equalTo("Kowalski"))
+                    .body("recipientAddress", notNullValue())
+                    .body("recipientAddress.country", equalTo("Poland"))
+                    .body("recipientAddress.city", equalTo("Lodz"))
+                    .body("recipientAddress.street", equalTo("Piotrkowska"))
+                    .body("recipientAddress.streetNumber", equalTo(1))
+                    .body("recipientAddress.postalCode", equalTo("90-000"))
+                    .body("accountLogin", equalTo("client"))
+                    .body("hash", notNullValue())
+                    .body("observed", equalTo(false))
+                    .body("totalPrice", equalTo(3699.99f));
+        }
+
+        @Order(2)
+        @DisplayName("Should properly get done order with filters")
+        @Test
+        void shouldProperlyGetDoneOrderWithFilter() {
+            given()
+                    .header(AUTHORIZATION, "Bearer " + AuthUtil.retrieveToken(SALES_REP))
+                    .when()
+                    .get("/order/filters?minPrice=3699&maxPrice=3700&amount=1")
+                    .then()
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .statusCode(200)
+                    .body("id", notNullValue())
+                    .body("orderedProducts", notNullValue())
+                    .body("orderedProducts.amount", notNullValue())
+                    .body("orderedProducts.productId", notNullValue())
+                    .body("orderState", notNullValue())
+                    .body("recipientFirstName", notNullValue())
+                    .body("recipientLastName", notNullValue())
+                    .body("recipientAddress", notNullValue())
+                    .body("recipientAddress.country", notNullValue())
+                    .body("recipientAddress.city", notNullValue())
+                    .body("recipientAddress.street", notNullValue())
+                    .body("recipientAddress.streetNumber", notNullValue())
+                    .body("recipientAddress.postalCode", notNullValue())
+                    .body("accountLogin", notNullValue())
+                    .body("hash", notNullValue())
+                    .body("observed", notNullValue())
+                    .body("totalPrice", notNullValue());
+        }
+        @Order(3)
+        @DisplayName("Should return empty list")
+        @Test
+        void shouldReturnEmptyList() {
+            given()
+                    .header(AUTHORIZATION, "Bearer " + AuthUtil.retrieveToken(SALES_REP))
+                    .when()
+                    .get("/order/filters?minPrice=3699&maxPrice=3700&amount=231231")
+                    .then()
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .statusCode(200)
+                    .body("size()",is(equalTo(0)));
+        }
+    }
+    @Nested
+    @Order(2)
+    @TestClassOrder(ClassOrderer.OrderAnnotation.class)
+    class Negative {
+        @Test
+        @DisplayName("Should fail if bearer is missing")
+        @Order(1)
+        void shouldFailIfBearerIsMissing() {
+            given()
+                    .when()
+                    .get("/order/filters?minPrice=3699&maxPrice=3700&amount=1")
+                    .then()
+                    .statusCode(401);
+        }
+    }
+}
--- src/test/java/pl/lodz/p/it/ssbd2023/ssbd02/web/controller/MOZ3IT.java
+++ src/test/java/pl/lodz/p/it/ssbd2023/ssbd02/web/controller/MOZ3IT.java
@@ -34,14 +34,13 @@ public class MOZ3IT {
     @Order(1)
     @TestClassOrder(ClassOrderer.OrderAnnotation.class)
     class Positive {
-
         @Order(1)
-        @DisplayName("Should properly create products")
+        @DisplayName("Should properly edit products")
         @Test
-        void shouldProperlyCreateProducts() {
+        void shouldProperlyEditProduct() {
             ProductCreateWithImageDto productCreateWithImageDto1 = ProductCreateWithImageDto.builder()
-                    .price(200.0)
-                    .amount(2)
+                    .price(400.0)
+                    .amount(5)
                     .weight(55.5)
                     .weightInPackage(60.5)
                     .furnitureWidth(300)
@@ -50,46 +49,31 @@ public class MOZ3IT {
                     .packageWidth(310)
                     .packageHeight(80)
                     .packageDepth(40)
-                    .color(Color.BROWN.name())
+                    .color(Color.RED.name())
                     .woodType(WoodType.BIRCH.name())
-                    .productGroupId(1L)
-                    .imageProductId(1L)
+                    .productGroupId(31L)
+                    .imageProductId(49L)
                     .build();
 
             ProductCreateWithImageDto productCreateWithImageDto2 = ProductCreateWithImageDto.builder()
-                    .price(300.0)
-                    .amount(2)
-                    .weight(55.5)
-                    .weightInPackage(60.5)
-                    .furnitureWidth(300)
-                    .furnitureHeight(70)
-                    .furnitureDepth(40)
-                    .packageWidth(310)
-                    .packageHeight(80)
-                    .packageDepth(40)
-                    .color(Color.BLACK.name())
+                    .price(499.0)
+                    .amount(5)
+                    .weight(53.5)
+                    .weightInPackage(62.5)
+                    .furnitureWidth(301)
+                    .furnitureHeight(80)
+                    .furnitureDepth(41)
+                    .packageWidth(312)
+                    .packageHeight(85)
+                    .packageDepth(43)
+                    .color(Color.RED.name())
                     .woodType(WoodType.BIRCH.name())
-                    .productGroupId(1L)
-                    .imageProductId(2L)
+                    .productGroupId(31L)
+                    .imageProductId(49L)
                     .build();
 
             productId1 = ProductUtil.createProductWithExistingImage(productCreateWithImageDto1);
             productId2 = ProductUtil.createProductWithExistingImage(productCreateWithImageDto2);
-        }
-
-        @Order(2)
-        @DisplayName("Should properly create products")
-        @Test
-        void shouldProperlyEditProduct() {
-            given()
-                    .when()
-                    .get("/product/id/" + productId1)
-                    .then()
-                    .contentType(MediaType.APPLICATION_JSON)
-                    .statusCode(200)
-                    .body("id", equalTo(productId1))
-                    .body("price", equalTo(200.0F))
-                    .body("amount", equalTo(2));
 
             EditProductDto editProductDto = InitData.getEditedProduct(productId1, 30.0, 40);
 
@@ -102,7 +86,6 @@ public class MOZ3IT {
                     .then()
                     .contentType(MediaType.APPLICATION_JSON)
                     .statusCode(200)
-                    .body("id", equalTo(productId1))
                     .body("price", equalTo(30.0F))
                     .body("amount", equalTo(40));
 
