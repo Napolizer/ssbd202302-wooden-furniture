a18528ab74b1d6bee19dc244ee7b914c68de8181
236534 Glapiński Michał <236534@edu.p.lodz.pl>
2023-06-22 07:58:00 +0200

    SSBD202302-455 Validation amount bugfix

--- src/main/java/pl/lodz/p/it/ssbd2023/ssbd02/annotations/validation/Amount.java
+++ src/main/java/pl/lodz/p/it/ssbd2023/ssbd02/annotations/validation/Amount.java
@@ -2,6 +2,7 @@ package pl.lodz.p.it.ssbd2023.ssbd02.annotations.validation;
 
 import jakarta.validation.Constraint;
 import jakarta.validation.Payload;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import java.lang.annotation.ElementType;
@@ -14,6 +15,7 @@ import java.lang.annotation.Target;
 @Constraint(validatedBy = {})
 @NotNull(message = "Field cannot be empty")
 @Pattern(regexp = "^[0-9]\\d*$", message = "Amount must be positive number")
+@Min(value = 0, message = "Amount must be at least {value}")
 public @interface Amount {
   String message() default "Amount must be positive number";
 
