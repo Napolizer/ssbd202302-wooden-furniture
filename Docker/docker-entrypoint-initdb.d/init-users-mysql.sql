7b319cad76d2c1557c979b0a6f8eae34ca01369a
Bartłomiej Dygasiński <bartek.dygasinski00@gmail.com>
2023-10-06 11:41:49 +0200

    Migrated scripts and created mysql container.

---
+++ Docker/docker-entrypoint-initdb.d/init-users-mysql.sql
@@ -0,0 +1,5 @@
+CREATE USER 'ssbd02mok'@'%' IDENTIFIED BY 'dbmok';
+CREATE USER 'ssbd02moz'@'%' IDENTIFIED BY 'dbmoz';
+CREATE USER 'ssbd02auth'@'%' IDENTIFIED BY 'dbauth';
+
+GRANT GRANT OPTION ON *.* TO 'ssbd02admin'@'%';
